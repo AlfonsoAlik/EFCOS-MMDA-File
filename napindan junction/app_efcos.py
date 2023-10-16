a3c98cd32685065bce27991e2ef4053f24bc7a18
Ariel Dominic Alfonso <53381880+AlfonsoAlik@users.noreply.github.com>
2023-10-16 14:44:35 +0800

    Add files via upload

--- napindan junction/app_efcos.py
+++ napindan junction/app_efcos.py
@@ -18,32 +18,33 @@ X = df.drop(['FLOOD_HEIGHT_HIGHEST'], axis=1)
 y= df['FLOOD_HEIGHT_HIGHEST']
 
 st.write("""
-# Metro Manila River Flood Prediction App
+# Metro Manila River Flood Prediction App for Napindan Junction
 """)
 
 st.sidebar.header('User Input Features')
 
 
 def user_input_features():
-    RAINFALL = st.sidebar.number_input('Rainfall', min_value=float(X.RAINFALL.min()),value=float(X.RAINFALL.mean()))
-    TMAX = st.sidebar.number_input('Tmax', min_value=float(X.TMAX.min()),value=float(X.TMAX.mean()))
-    TMIN = st.sidebar.number_input('Tmin', min_value=float(X.TMIN.min()),value=float(X.TMIN.mean()))
-    TMEAN = st.sidebar.number_input('Tmean', min_value=float(X.TMEAN.min()),value=float(X.TMEAN.mean()))
-    WIND_SPEED = st.sidebar.number_input('Wind speed', min_value=float(X.WIND_SPEED.min()),value=2.0)
-    WIND_DIRECTION = st.sidebar.number_input('Wind direction', min_value=float(X.WIND_DIRECTION.min()),value=180.0)
-    FLOOD_HEIGHT_LOWEST = st.sidebar.number_input('Flood Height Lowest', min_value=float(X.FLOOD_HEIGHT_LOWEST.min()),value=float(X.FLOOD_HEIGHT_LOWEST.mean()))
-    FLOOD__HEIGHT_AVE = st.sidebar.number_input('Flood Height Average',min_value=float(X.FLOOD__HEIGHT_AVE.min()),value=float(X.FLOOD__HEIGHT_AVE.mean()))
+    RAINFALL = st.sidebar.number_input('Rainfall (mm)', min_value=float(X.RAINFALL.min()),value=float(X.RAINFALL.mean()))
+    TMAX = st.sidebar.number_input('Tmax (Celsius °C)', min_value=float(X.TMAX.min()),value=float(X.TMAX.mean()))
+    TMIN = st.sidebar.number_input('Tmin (Celsius °C)', min_value=float(X.TMIN.min()),value=float(X.TMIN.mean()))
+    TMEAN = st.sidebar.number_input('Tmean (Celsius °C)', min_value=float(X.TMEAN.min()),value=float(X.TMEAN.mean()))
+    WIND_SPEED = st.sidebar.number_input('Wind speed (m/s)', min_value=float(X.WIND_SPEED.min()),value=2.0)
+    WIND_DIRECTION = st.sidebar.number_input('Wind direction (degrees)', min_value=float(X.WIND_DIRECTION.min()),value=180.0)
+    FLOOD_HEIGHT_LOWEST = st.sidebar.number_input('Flood Height Lowest (EL.m)', min_value=float(X.FLOOD_HEIGHT_LOWEST.min()),value=float(X.FLOOD_HEIGHT_LOWEST.mean()))
+    FLOOD__HEIGHT_AVE = st.sidebar.number_input('Flood Height Average (EL.m)',min_value=float(X.FLOOD__HEIGHT_AVE.min()),value=float(X.FLOOD__HEIGHT_AVE.mean()))
 
     data = {
-            'RAINFALL': RAINFALL,
-            'TMAX': TMAX,
-            'TMIN': TMIN,
-            'TMEAN': TMEAN,
-            'WIND_SPEED': WIND_SPEED,
-            'WIND_DIRECTION': WIND_DIRECTION,
-            'FLOOD_HEIGHT_LOWEST': FLOOD_HEIGHT_LOWEST,
-            'FLOOD__HEIGHT_AVE': FLOOD__HEIGHT_AVE,
+            'RAINFALL (mm)': RAINFALL,
+            'TMAX (Celsius °C)': TMAX,
+            'TMIN (Celsius °C)': TMIN,
+            'TMEAN (Celsius °C)': TMEAN,
+            'WIND_SPEED (m/s)': WIND_SPEED,
+            'WIND_DIRECTION (degrees)': WIND_DIRECTION,
+            'FLOOD_HEIGHT_LOWEST (EL.m)': FLOOD_HEIGHT_LOWEST,
+            'FLOOD__HEIGHT_AVE (EL.m)': FLOOD__HEIGHT_AVE,
             }
+
 
     features = pd.DataFrame(data, index=[0])
     return features
